d1a052d90ab2f2fe7e0ae6af12726707751abb5c
Julian Vargas <julianvargasalvarez@gmail.com>
2013-05-28 14:34:27 -0500

    Adiciona pruebas para modelos de profesor, acudiente y estudiante

--- pejelagarto/models.py
+++ pejelagarto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Estudiante(models.Model):
     edad = models.IntegerField()
+    nombre = models.CharField(max_length=30)
 
     def es_mayor_de_edad(self):
         return self.edad > 17
--- pejelagarto/tests.py
+++ pejelagarto/tests.py
@@ -1,6 +1,47 @@
 from django.test import TestCase
 from models import Estudiante
 
+class DatosBasicosProfesorRequeridosTest(TestCase):
+    def test_longitud_nombre_mayor_a_cero(self):
+        """
+        Prueba que el nombre ingresado tenga una
+        longitud mayor a cero.
+        """
+        profesor = Profesor()
+        profesor.nombre = "Julian"
+        profesor.save()
+
+        p = Profesor.objects.all()[0]
+        self.assertEqual(p.nombre, "Julian")
+
+class DatosBasicosAcudienteRequeridosTest(TestCase):
+    def test_longitud_nombre_mayor_a_cero(self):
+        """
+        Prueba que el nombre ingresado tenga una
+        longitud mayor a cero.
+        """
+        acudiente = Acudiente()
+        acudiente.edad = 0
+        acudiente.nombre = "Julian"
+        acudiente.save()
+
+        a = Acudiente.objects.all()[0]
+        self.assertEqual(a.nombre, "Julian")
+
+class DatosBasicosEstudianteRequeridosTest(TestCase):
+    def test_longitud_nombre_mayor_a_cero(self):
+        """
+        Prueba que el nombre ingresado tenga una
+        longitud mayor a cero.
+        """
+        estudiante = Estudiante()
+        estudiante.edad = 0
+        estudiante.nombre = "Julian"
+        estudiante.save()
+
+        e = Estudiante.objects.all()[0]
+        self.assertEqual(e.nombre, "Julian")
+
 class MayorEdadTest(TestCase):
     def test_es_mayor_de_edad_con_18(self):
         """

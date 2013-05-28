7958dcd1c5957cb2289a00164e912244fbb626dc
luis gonzalez <luisescorpion-19@hotmail.com>
2013-05-28 11:15:48 -0500

    commit

--- pila/settings.py
+++ pila/settings.py
@@ -1,18 +1,22 @@
 # Django settings for pila project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+# Project directory
+PROJECT_DIR = os.path.dirname(__file__)
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
+autocmd File Type text setlocal textwidth=78
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, 'pejelagarto'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -120,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pejelagarto',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

32caf68d9c964022a3ecb8c28e6cd60c3d7e3648
Juan Alvarez <juangabo.0228@gmail.com>
2013-05-28 12:22:32 -0500

    Crea pruebas unitarias para mayor de edad
    Se crearon las pruebas unitarias para Estudiante#es_mayor_de_edad

--- pejelagarto/tests.py
+++ pejelagarto/tests.py
@@ -1,16 +1,29 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+class MayorEdadTest(TestCase):
+    def test_es_mayor_de_edad_con_18(self):
+        """
+        Prueba que el estudiante es mayor de edad
+        cuando la edad es 18.
+        """
+        estudiante = Estudiante()
+        estudiante.edad = 18
+        self.assertEqual(estudiante.es_mayor_de_edad(), True)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_es_mayor_de_edad_con_mas_de_18(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Prueba que el estudiante es mayor de edad
+        cuando la edad es > 18.
         """
-        self.assertEqual(1 + 1, 2)
+        estudiante = Estudiante()
+        estudiante.edad = 19
+        self.assertEqual(estudiante.es_mayor_de_edad(), True)
+
+    def test_no_es_mayor_de_edad_con_menos_de_18(self):
+        """
+        Prueba que el estudiante no es mayor de edad
+        cuando la edad es < 18.
+        """
+        estudiante = Estudiante()
+        estudiante.edad = 17
+        self.assertEqual(estudiante.es_mayor_de_edad(), False)
